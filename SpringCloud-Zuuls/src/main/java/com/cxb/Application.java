37749c472bbbb7c8733e4a35026ca6cfd6a95b79
chenxiban <chen867647213@163.com>
2018-12-09 17:28:42 +0800

    Zuul网关!!!

--- SpringCloud-Zuuls/src/main/java/com/cxb/Application.java
+++ SpringCloud-Zuuls/src/main/java/com/cxb/Application.java
@@ -3,9 +3,15 @@ package com.cxb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * 启动类
+ * 
  * @Description:
  * @ClassName: Application.java
  * @author Chenyongjia
@@ -13,9 +19,35 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @Email 867647213@qq.com
  */
 @EnableZuulProxy
+@EnableAuthorizationServer
 @SpringCloudApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	/**
+	 * 
+	 * attention:简单跨域就是GET，HEAD和POST请求，但是POST请求的"Content-Type"只能是application/x-www-form-urlencoded,
+	 * multipart/form-data 或 text/plain
+	 * 反之，就是非简单跨域，此跨域有一个预检机制，说直白点，就是会发两次请求，一次OPTIONS请求，一次真正的请求
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.setMaxAge(18000L);
+		config.addAllowedMethod("OPTIONS");// 允许提交请求的方法，*表示全部允许
+		config.addAllowedMethod("GET");// 允许Get的请求方法
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+	
 }
