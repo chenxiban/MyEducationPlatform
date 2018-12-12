33c657e0556dcd06a6f0b81f39162a93113ed313
WANGMENGXIA <2934128615@qq.com>
2018-12-12 18:51:43 +0800

    ....

--- FileManagement/src/main/java/com/cxb/Application.java
+++ FileManagement/src/main/java/com/cxb/Application.java
@@ -1,5 +1,26 @@
 package com.cxb;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * 
+ * @Description: SpringCloud微服务启动类
+ * @ClassName: Application.java
+ * @author ChenYongJia
+ * @Date 2018年12月01日 晚上22：54
+ * @Email chen87647213@163.com
+ */
+@SpringCloudApplication // ===( @EnableEurekaClient + @EnableHystrix + @SpringBootApplication
+// )pom.xml必须引Eureka、Hystrix依赖
+@EnableAuthorizationServer //
+@EnableFeignClients
+@MapperScan("com.cxb.cyj.dao")// 指定扫描的mapper接口所在的包
 public class Application {
-	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
