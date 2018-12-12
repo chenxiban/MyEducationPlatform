3fb8042545917f715a2fb88f7c091a1d77af4b81
WANGMENGXIA <2934128615@qq.com>
2018-12-12 18:52:15 +0800

    王梦霞拉取李

--- LiHaiChao/src/main/java/com/cxb/Application.java
+++ LiHaiChao/src/main/java/com/cxb/Application.java
@@ -1,5 +1,14 @@
-package com.cxb;
+package com.cxb; 
+  
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
-public class Application {
 
+@SpringCloudApplication
+@FeignClient
+public class Application { 
+    public static void main(String[] args) { 
+        SpringApplication.run(Application.class,args);// 程序启动入口 	启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+    }
 }
