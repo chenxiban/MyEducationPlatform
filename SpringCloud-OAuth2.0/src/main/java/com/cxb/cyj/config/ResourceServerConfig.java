1745ca6859ca75f98428f4bdae2e910834fa11fa
chenxiban <chen867647213@163.com>
2018-12-13 13:14:14 +0800

    陈永佳:更新OAuth2.0密码认证模式项目,更新时间:2018-12-13 13:13!!!

---
+++ SpringCloud-OAuth2.0/src/main/java/com/cxb/cyj/config/ResourceServerConfig.java
@@ -0,0 +1,23 @@
+package com.cxb.cyj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * 
+ * @Description: 因为我们认证中心会提供User信息，所以也是资源服务器。
+ * @ClassName: ResourceServerConfig.java
+ * @author ChenYongJia
+ * @Date 2018年12月04日 下午20:40:56
+ * @Email 867647213@qq.com
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/api/order/**").authenticated();
+	}
+}
