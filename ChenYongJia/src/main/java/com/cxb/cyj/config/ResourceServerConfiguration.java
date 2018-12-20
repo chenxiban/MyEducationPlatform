0c4d903c1013dc3a5fca15f5b351bd24def3c0e4
chenxiban <chen867647213@163.com>
2018-12-20 15:34:20 +0800

    陈永佳:更新陈永佳组ChenYongJia项目,更新时间:2018-12-20 15:34!!!

--- ChenYongJia/src/main/java/com/cxb/cyj/config/ResourceServerConfiguration.java
+++ ChenYongJia/src/main/java/com/cxb/cyj/config/ResourceServerConfiguration.java
@@ -1,4 +1,4 @@
-package com.cxb.cyj.config;
+package com.cxb.cyj.config; 
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
